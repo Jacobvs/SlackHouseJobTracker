3fcacff05c68244d145eff7165d24c5e03fdf0d1
Jacob van Steyn <jacobvs2@gmail.com>
2022-01-15 18:42:28 -0500

    Initial Commit

---
+++ helpers.py
@@ -0,0 +1,112 @@
+import json
+import slack_sdk.models
+
+def add_user_blocks(user_name: str, user_id: str, enabled: bool, job_name: str, job_days: str):
+    data = json.load(open('base_user_view.json'))
+    data[0]['fields'][0]['text'] = f"*{user_name}*"
+    data[0]['fields'][1]['text'] = f"Enabled:\t:white_check_mark:" if enabled else f"Enabled:\t:x:"
+    data[0]['accessory']['action_id'] = f"{user_id}_edit_user"
+    data[0]['accessory']['value'] = user_id
+
+    data[1]['elements'][0]['text'] = f"Job Name: {job_name}"
+    data[1]['elements'][1]['text'] = f"Days: {job_days}"
+    return data
+
+
+def generate_users_modal(user_data: list):
+    data = json.load(open('base_edit_modal.json'))
+    user_views = [
+        add_user_blocks(user['user_name'], user['user_id'], user['enabled'], user['job_name'], user['job_days'])
+        for user in user_data]
+
+    for user_view in reversed(user_views):
+        data['blocks'].append(user_view[0])
+        data['blocks'].append(user_view[1])
+
+    return data
+
+
+def generate_edit_modal(user_name: str, user_id: str, enabled: bool, job_name: str, job_days: list):
+    data = json.load(open('user_edit_modal.json'))
+    data['title']['text'] = f"Edit {user_name}"
+    data['blocks'][1]['element']['initial_value'] = job_name
+
+    for day in job_days:
+        data['blocks'][2]['accessory']['initial_options'].append(
+            {
+                "text": {
+                    "type": "plain_text",
+                    "text": f"{day}",
+                    "emoji": True
+                },
+                "value": f"{day}"
+            }
+        )
+
+    data['blocks'][3]['accessory']['initial_option'] = {
+            "text": {
+                "type": "plain_text",
+                "text": "Active",
+                "emoji": True
+            },
+            "value": "Active"
+        } if enabled else {
+            "text": {
+                "type": "plain_text",
+                "text": "Inactive",
+                "emoji": True
+            },
+            "value": "Inactive"
+        }
+
+    data['private_metadata'] = user_id
+
+    return data
+
+
+def get_userdata(client: slack_sdk.WebClient):
+    userlist = client.users_list()['members']
+
+    all_uids = [user.id for user in userlist]
+    populate_userdata(all_uids)
+
+    jobdata = json.load(open('jobdata.json'))
+
+    data = []
+    for user in userlist:
+        udata = {
+            'user_name': user['profile']['real_name'],
+            'user_id': user['id'],
+            'enabled': jobdata[user['id']]['enabled'],
+            'job_name': jobdata[user['id']]['job_name'],
+            'job_days': jobdata[user['id']]['days'].join(', ')
+        }
+        data.append(udata)
+
+    return data
+
+
+def populate_userdata(all_uids: list):
+    data: dict = json.load('jobdata.json')
+    for key in all_uids:
+        if key not in data:
+            data[key] = {
+                'enabled': False,
+                'job_name': 'N/A',
+                'days': []
+            }
+    json.dump(data, open('jobdata.json', 'w'), indent=4)
+    return data
+
+def save_userdata(user_id: str, enabled: bool, job_name: str, job_days: list):
+    data = json.load(open('jobdata.json'))
+    data[user_id]['enabled'] = enabled
+    data[user_id]['job_name'] = job_name
+    data[user_id]['days'] = sort_days(job_days)
+    json.dump(data, open('jobdata.json', 'w'), indent=4)
+
+def sort_days(days: list):
+    days.sort(key=lambda x: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'].index(x))
+    return days
+
+
---
+++ main.py
@@ -0,0 +1,67 @@
+import os
+import logging
+# Use the package we installed
+from slack_bolt import App
+import helpers
+
+logging.basicConfig(level=logging.DEBUG)
+
+# Set house manager uid (In slack: view full profile -> more -> copy member id)
+HOUSE_MANAGER_UID = os.environ.get("HOUSE_MANAGER_UID")
+DEVELOPER_UID = os.environ.get("DEVELOPER_UID")
+
+# Initializes your app with your bot token and signing secret
+app = App(
+    token=os.environ.get("SLACK_BOT_TOKEN"),
+    signing_secret=os.environ.get("SLACK_SIGNING_SECRET")
+)
+
+@app.middleware  # or app.use(log_request)
+def log_request(logger, body, next):
+    logger.debug(body)
+    return next()
+
+
+@app.command('/configurejobs')
+def configure_jobs(body, client, ack, logger):
+    user_id = body["user_id"]
+    if user_id not in [HOUSE_MANAGER_UID, DEVELOPER_UID]:
+        return ack("You are not authorized to configure jobs.")
+    ack(f"Hi <@{user_id}>!\nYou can configure house jobs in the popup panel.")
+    # Send interactive message to the user
+    # TODO: refresh user list with any missing users
+    # TODO: Buttons - previous, next, done, cancel
+    userdata = helpers.get_userdata(client)
+    res = client.views_open(
+        trigger_id=body["trigger_id"],
+        view=helpers.generate_users_modal(userdata)
+    )
+    logger.info(res)
+
+@app.view('user_edit_modal_submit')
+def user_edit_modal_submit(body, client, logger):
+    values = body["view"]["state"]["values"]
+    logger.info(values)
+    uid = body["view"]["private_metadata"]
+    enabled = values['jobstatus']['selected'] == 'Active'
+    job_name = values['jobname']
+    job_days = values['days']['selected']
+    helpers.save_userdata(uid, enabled, job_name, job_days)
+
+
+@app.event("app_mention")
+def event_test(body, say, logger):
+    logger.info(body)
+    say("Do your house job already and stop bothering me :clown:")
+
+@app.error
+def global_error_handler(error, body, logger):
+    logger.exception(error)
+    logger.info(body)
+
+
+
+
+# Start your app
+if __name__ == "__main__":
+    app.start(3000)
